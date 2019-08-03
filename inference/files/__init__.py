af94b9988c887c9f580be53d8f325373ab1cd060
Michael Pound <michael.pound@nottingham.ac.uk>
2019-08-03 23:26:10 +0100

    Obsolete code removal

--- inference/files/__init__.py
+++ inference/files/__init__.py
@@ -1,4 +1,2 @@
-from files.utils import convert_state_dict
-from files.func import nonmaximalsuppression, draw_labelmaps, color_heatmap, neighbors
-from files.rrtree import rrtree
+from files.func import nonmaximalsuppression, rrtree
 from files.image_proc import distance_map, image_output, decode_segmap, distance_to_weights
